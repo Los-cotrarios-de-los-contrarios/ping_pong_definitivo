3352358201918796f4c8cc39784b57dd351c072f
clauloro <clauloro@myuax.com>
2022-12-01 19:17:34 +0100

    reinicio de la pelota

--- juego.py
+++ juego.py
@@ -1,8 +1,53 @@
-# pong_1_5.py: Clase RaquetaPong
-from modulo_pelota import*
+# pong_1_6.py: Mover raqueta con teclado
+
 import random
 import pygame
 from pygame.locals import QUIT
+
+# Constantes para la inicialización de la superficie de dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
+
+
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # --- Atributos de la Clase ---
+
+        # Imagen de la Pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+
+    def rebotar(self):
+        if self.x <= -self.ancho:
+            self.reiniciar()
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
 
 
 class RaquetaPong:
@@ -23,6 +68,10 @@ class RaquetaPong:
 
     def mover(self):
         self.y += self.dir_y
+        if self.y <= 0:
+            self.y = 0
+        if self.y + self.alto >= VENTANA_VERT:
+            self.y = VENTANA_VERT - self.alto
 
 
 def main():
@@ -31,7 +80,7 @@ def main():
 
     # Inicialización de la superficie de dibujo (display surface)
     ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
-    pygame.display.set_caption("Pong 5")
+    pygame.display.set_caption("Pong 6")
 
     pelota = PelotaPong("bola_roja.png")
 
@@ -46,6 +95,7 @@ def main():
     while jugando:
         pelota.mover()
         pelota.rebotar()
+        raqueta_1.mover()
 
         ventana.fill(BLANCO)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
@@ -56,6 +106,20 @@ def main():
             if event.type == QUIT:
                 jugando = False
 
+            # Detecta que se ha pulsado una tecla
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = -5
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 5
+
+            # Detecta que se ha soltado la tecla
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = 0
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 0
+
         pygame.display.flip()
         pygame.time.Clock().tick(FPS)
 
@@ -64,3 +128,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
