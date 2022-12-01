3e99139943e538c9e6a3d59cb3f153bedf9d6014
clauloro <clauloro@myuax.com>
2022-12-01 18:59:07 +0100

    organizo codigo por modulos

--- juego.py
+++ juego.py
@@ -1,53 +1,8 @@
 # pong_1_5.py: Clase RaquetaPong
-
+from modulo_pelota import*
 import random
 import pygame
 from pygame.locals import QUIT
-
-# Constantes para la inicialización de la superficie de dibujo
-VENTANA_HORI = 800  # Ancho de la ventana
-VENTANA_VERT = 600  # Alto de la ventana
-FPS = 60  # Fotogramas por segundo
-BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
-
-
-class PelotaPong:
-    def __init__(self, fichero_imagen):
-        # --- Atributos de la Clase ---
-
-        # Imagen de la Pelota
-        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
-
-        # Dimensiones de la Pelota
-        self.ancho, self.alto = self.imagen.get_size()
-
-        # Posición de la Pelota
-        self.x = VENTANA_HORI / 2 - self.ancho / 2
-        self.y = VENTANA_VERT / 2 - self.alto / 2
-
-        # Dirección de movimiento de la Pelota
-        self.dir_x = random.choice([-5, 5])
-        self.dir_y = random.choice([-5, 5])
-
-    def mover(self):
-        self.x += self.dir_x
-        self.y += self.dir_y
-
-    def rebotar(self):
-        if self.x <= -self.ancho:
-            self.reiniciar()
-        if self.x >= VENTANA_HORI:
-            self.reiniciar()
-        if self.y <= 0:
-            self.dir_y = -self.dir_y
-        if self.y + self.alto >= VENTANA_VERT:
-            self.dir_y = -self.dir_y
-
-    def reiniciar(self):
-        self.x = VENTANA_HORI / 2 - self.ancho / 2
-        self.y = VENTANA_VERT / 2 - self.alto / 2
-        self.dir_x = -self.dir_x
-        self.dir_y = random.choice([-5, 5])
 
 
 class RaquetaPong:
